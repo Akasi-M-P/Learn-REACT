8881cc08f550c9b7d2a413baf5ec56f24e3a2e76
Mba96 <mbajameel90@gmail.com>
2023-03-16 22:53:29 +0000

    initial commit of my first react project and learning how to import images and css to a react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./index.css";
+
+const MovieList = () => {
+  return (
+    <section className="movielist">
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+    </section>
+  );
+};
+
+const Movie = () => {
+  return (
+    <section className="movie">
+      <Image />
+      <Title />
+      <Director />
+    </section>
+  );
+};
+
+const Image = () => (
+  <img src="./images/movie-1.jpg" alt="	Luther: The Fallen Sun" />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h2> Luther: The Fallen Sun</h2>;
+
+const Director = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>Jamie Payne</h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<MovieList />);
